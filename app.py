93e4bb101ee9eb9bd9bf442b98b162f7f99a70e9
Christian <chr.hagel@gmail.com>
2016-12-31 22:00:42 -0800

    pandas and numpy

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask import request, render_template, jsonify
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    numse = np.array([1, 2, 3])
     tvar = 'bob'
 
     return render_template('index.html', tvar = tvar)

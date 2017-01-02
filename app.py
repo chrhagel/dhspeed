314c6183f7cd746f3411d9254d1fb6a4429c8a0f
Christian <chr.hagel@gmail.com>
2017-01-02 13:39:07 -0800

    added blob storage test

--- app.py
+++ app.py
@@ -2,13 +2,23 @@ from flask import Flask
 from flask import request, render_template, jsonify
 import pandas as pd
 import numpy as np
+from azure.storage.blob import BlockBlobService
+
+
+account = 'dhspeed'
+account_key = 'gePaxovANJMYSzIEnIWCD0aJPbbeogEjC8NfuJwvuAhKMsaOMwZbeWEr1C4hMqpunca/obB4LlwPlTqfr4EZgg=='
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    numse = np.array([1, 2, 3])
     tvar = 'bob'
+
+    block_blob_service = BlockBlobService(account_name=account, account_key=account_key)
+    block_blob_service.get_blob_to_path('rides', 'id_1_calibrated.csv', 'azure_out_test.csv')
+
+    ride = pd.read_csv('azure_out_test.csv')
 
     return render_template('index.html', tvar = tvar)
 
